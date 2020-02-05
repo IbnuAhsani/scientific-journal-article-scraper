276d7184f9bd76d1bdd8e3acaa3fc3adc218bad8
IbnuAhsani <ibnu.muhari@gmail.com>
2020-02-05 10:47:33 +0700

    feat: extract abstracts from a single page

---
+++ web-crawler.py
@@ -0,0 +1,21 @@
+import requests
+from bs4 import BeautifulSoup
+
+
+def abstract_spider(max_pages):
+    page = 1
+    url = 'http://garuda.ristekdikti.go.id/journal/view/11578'
+
+    while page <= max_pages:
+        html_page_source_code = requests.get(url)
+        html_page_plain_text = html_page_source_code.text
+        soup = BeautifulSoup(html_page_plain_text, "html.parser")
+
+        for xmp in soup.findAll('xmp', {'class': 'abstract-article'}):
+            print(xmp.string)
+            print('\n')
+
+        page += 1
+
+
+abstract_spider(1)
