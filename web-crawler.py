693da98a7aeb01d0cccca09ea0c31f9369ade9e8
IbnuAhsani <ibnu.muhari@gmail.com>
2020-02-05 11:07:19 +0700

    feat: page iterator

--- web-crawler.py
+++ web-crawler.py
@@ -5,6 +5,7 @@ from bs4 import BeautifulSoup
 def abstract_spider(max_pages):
     page = 1
     url = 'http://garuda.ristekdikti.go.id/journal/view/11578'
+    separator = '?page='
 
     while page <= max_pages:
         html_page_source_code = requests.get(url)
@@ -17,5 +18,11 @@ def abstract_spider(max_pages):
 
         page += 1
 
+        if page == 2:
+            url += separator + str(page)
+        else:
+            url = url.split(separator, 1)[0]
+            url += separator + str(page)
 
-abstract_spider(1)
+
+abstract_spider(3)
