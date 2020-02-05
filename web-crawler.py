5d842a5c16e1ada51377fd13f5d379784446b89a
IbnuAhsani <ibnu.muhari@gmail.com>
2020-02-05 12:21:47 +0700

    feat: get the max pages for each jurnal

--- web-crawler.py
+++ web-crawler.py
@@ -1,28 +1,35 @@
+import re
 import requests
 from bs4 import BeautifulSoup
 
 
-def abstract_spider(max_pages):
-    page = 1
+def abstract_spider():
+    current_page = 1
     url = 'http://garuda.ristekdikti.go.id/journal/view/11578'
     separator = '?page='
 
-    while page <= max_pages:
-        html_page_source_code = requests.get(url)
-        html_page_plain_text = html_page_source_code.text
-        soup = BeautifulSoup(html_page_plain_text, "html.parser")
+    html_page_source_code_string = requests.get(url).text
+    soup = BeautifulSoup(html_page_source_code_string, 'html.parser')
+    pagination_info_string = soup.find(
+        'p', {'class': 'pagination-info'}).string.replace(" ", "")
+
+    max_pages = int(re.search('of(.*)\\|', pagination_info_string).group(1))
+
+    while current_page <= max_pages:
+        html_page_source_code_string = requests.get(url).text
+        soup = BeautifulSoup(html_page_source_code_string, 'html.parser')
 
         for xmp in soup.findAll('xmp', {'class': 'abstract-article'}):
             print(xmp.string)
             print('\n')
 
-        page += 1
+        current_page += 1
 
-        if page == 2:
-            url += separator + str(page)
+        if current_page == 2:
+            url += separator + str(current_page)
         else:
             url = url.split(separator, 1)[0]
-            url += separator + str(page)
+            url += separator + str(current_page)
 
 
-abstract_spider(3)
+abstract_spider()
