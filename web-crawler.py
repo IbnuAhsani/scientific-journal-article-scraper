f3b850babc7e9c433953ec5cacfa0cb38e598445
IbnuAhsani <ibnu.muhari@gmail.com>
2020-02-05 15:55:58 +0700

    feat: iterate main journal page & access each journal

--- web-crawler.py
+++ web-crawler.py
@@ -3,9 +3,8 @@ import requests
 from bs4 import BeautifulSoup
 
 
-def abstract_spider():
+def abstract_spider(url):
     current_page = 1
-    url = 'http://garuda.ristekdikti.go.id/journal/view/11578'
     separator = '?page='
 
     html_page_source_code_string = requests.get(url).text
@@ -32,4 +31,29 @@ def abstract_spider():
             url += separator + str(current_page)
 
 
-abstract_spider()
+def journal_spider():
+    current_page = 1
+    base_url = 'http://garuda.ristekdikti.go.id'
+    main_journal_url = base_url + '/journal'
+    separator = '?page='
+
+    html_page_source_code_string = requests.get(main_journal_url).text
+    soup = BeautifulSoup(html_page_source_code_string, 'html.parser')
+    pagination_info_string = soup.find(
+        'p', {'class': 'pagination-info'}).string.replace(" ", "")
+
+    max_pages = int(re.search('of(.*)\\|', pagination_info_string).group(1))
+
+    while current_page <= max_pages:
+        html_page_source_code_string = requests.get(main_journal_url).text
+        soup = BeautifulSoup(html_page_source_code_string, 'html.parser')
+
+        for a in soup.findAll('a', {'class': 'title-journal'}):
+            journal_endpoint = a.get('href')
+            specific_journal_url = base_url + journal_endpoint
+            abstract_spider(specific_journal_url)
+
+        current_page += 1
+
+
+journal_spider()
