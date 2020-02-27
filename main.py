664e5c859bc49031883fd830407beef01839d45a
IbnuAhsani <ibnu.muhari@gmail.com>
2020-02-27 13:36:48 +0700

    feat: add path params to save_as_csv func

--- main.py
+++ main.py
@@ -27,7 +27,9 @@ def main():
     # print article_list in a pretty manner
     # pprint(article_list)
 
-    is_articles_saved = fs.save_articles_csv(article_list)
+    save_path = "./output/output.csv"
+
+    is_articles_saved = fs.save_articles_csv(save_path, article_list)
 
     if is_articles_saved is True:
         print('| Articles have been saved as .csv')
--- utils/file_system.py
+++ utils/file_system.py
@@ -1,11 +1,11 @@
 import csv
 
 
-def save_articles_csv(article_list):
+def save_articles_csv(path, article_list):
     is_articles_saved = True
 
     try:
-        with open("output.csv", "w") as csvfile:
+        with open(path, "w") as csvfile:
             writer = csv.writer(csvfile)
 
             for article in article_list:
