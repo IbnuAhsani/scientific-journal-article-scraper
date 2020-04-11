cd290edb8fcb892d6f915b6912820dbacc69cbe1
IbnuAhsani <ibnu.muhari@gmail.com>
2020-04-11 17:25:16 +0700

    feat: filter journals by their subjects

---
+++ journal_filter_by_subject.py
@@ -0,0 +1,48 @@
+from operator import itemgetter
+from utils import file_system as fs
+
+
+DATASET_PATH = "./output/dataset-test.csv"
+JOURNAL_SUBJECTS_PATH = "./output/journal-subject-cs-math.csv"
+SAVE_PATH = "./output/output-filtered-by-subject.csv"
+
+
+def main():
+
+    subject_list = []
+    csv_subject_data = fs.read_csv_list(JOURNAL_SUBJECTS_PATH)
+
+    for i in range(len(csv_subject_data)):
+        row = csv_subject_data[i]
+
+        journal_title, journal_subjects = itemgetter(
+            'JOURNAL_TITLE', 'JOURNAL_SUBJECTS')(row)
+
+        subject_list.append(journal_title)
+
+    journal_list = []
+    csv_header = ['SINTA_INDEX', 'JOURNAL_TITLE',
+                  'ARTICLE_TITLE', 'ARTICLE_ABSTRACT']
+
+    journal_list.append(csv_header)
+
+    csv_journal_data = fs.read_csv_list(DATASET_PATH)
+
+    for i in range(len(csv_journal_data)):
+        row = csv_journal_data[i]
+
+        sinta_index, journal_title, article_title, article_abstract = itemgetter(
+            'SINTA_INDEX', 'JOURNAL_TITLE', 'ARTICLE_TITLE', 'ARTICLE_ABSTRACT')(row)
+
+        is_journal_included = journal_title in subject_list
+
+        if is_journal_included is True:
+            journal = [sinta_index, journal_title,
+                       article_title, article_abstract]
+            journal_list.append(journal)
+
+    is_articles_saved = fs.save_articles_csv(SAVE_PATH, journal_list)
+
+
+if __name__ == '__main__':
+    main()
